1297a564b0fc4419e0c82d08be84b887cdffadd5
Family <albon.136546120855@2depedqc.ph>
2023-11-06 19:10:52 +0800

    Area of a Triangle

--- Flask_Intro/app.py
+++ Flask_Intro/app.py
@@ -11,12 +11,20 @@ def profile():
     return render_template('profile.html')
 
 @app.route('/works', methods=['GET', 'POST'])
-def works():
+def works1():
     result = None
     if request.method == 'POST':
         input_string = request.form.get('inputString', '')
         result = input_string.upper()
     return render_template('touppercase.html', result=result)
+
+@app.route('/areaOfcirle', methods=['GET', 'POST'])
+def works2():
+    result = None
+    if request.method == 'POST':
+        input_radius = request.form.get('value', '')
+        result = 3.14 * (float(input_radius) ** 2)
+    return render_template('circular_area.html', result=result)
 
 @app.route('/contact')
 def contact():
